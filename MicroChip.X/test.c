2e4e0e8be86fc21e19fb6f38510793a902b265b5
Xx-oX <ycwu4142@gmail.com>
2021-01-13 18:41:34 +0800

    add test.c

--- MicroChip.X/test.c
+++ MicroChip.X/test.c
@@ -1,82 +1,107 @@
-/*
- * File:   main.c
- *
- * Created on 2021年1月13日, 上午 9:58
- */
+// PIC18F4520 Configuration Bit Settings
 
-/* configuration */
-#pragma config OSC = INTIO67
-#pragma config FCMEN = OFF 
-#pragma config IESO = OFF
-// #pragma config2L
-#pragma config PWRT = OFF
-#pragma config BOREN = SBORDIS
-#pragma config BORV = 3
-// #pragma config2H
-#pragma config WDT = OFF
-#pragma config WDTPS = 32768
-// #pragma config3H
-#pragma config CCP2MX = PORTC
-#pragma config PBADEN = OFF
-#pragma config LPT1OSC = OFF
-#pragma config MCLRE = ON
-// #pragma config4L
-#pragma config STVREN = ON
-#pragma config LVP = OFF
-#pragma config XINST = OFF
-// #pragma config5L
-#pragma config CP0 = OFF
-#pragma config CP1 = OFF
-#pragma config CP2 = OFF
-#pragma config CP3 = OFF
-// #pragma config5H
-#pragma config CPB = OFF
-#pragma config CPD = OFF
-// #pragma config6L
-#pragma config WRT0 = OFF
-#pragma config WRT1 = OFF
-#pragma config WRT2 = OFF
-#pragma config WRT3 = OFF
-// #pragma config6H
-#pragma config WRTC = OFF
-#pragma config WRTB = OFF
-#pragma config WRTD = OFF
-// #pragma config7L
-#pragma config EBTR0 = OFF
-#pragma config EBTR1 = OFF
-#pragma config EBTR2 = OFF
-#pragma config EBTR3 = OFF 
-// #pragma config7H
-#pragma config EBTRB = OFF
+// 'C' source line config statements
 
+// CONFIG1H
+#pragma config OSC = HS         // Oscillator Selection bits (HS oscillator)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
 
-/* includes */      
+// CONFIG2L
+#pragma config PWRT = OFF       // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = SBORDIS  // Brown-out Reset Enable bits (Brown-out Reset enabled in hardware only (SBOREN is disabled))
+#pragma config BORV = 3         // Brown Out Reset Voltage bits (Minimum setting)
+
+// CONFIG2H
+#pragma config WDT = OFF        // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+#pragma config WDTPS = 32768    // Watchdog Timer Postscale Select bits (1:32768)
+
+// CONFIG3H
+#pragma config CCP2MX = PORTC   // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+#pragma config PBADEN = ON      // PORTB A/D Enable bit (PORTB<4:0> pins are configured as analog input channels on Reset)
+#pragma config LPT1OSC = OFF    // Low-Power Timer1 Oscillator Enable bit (Timer1 configured for higher power operation)
+#pragma config MCLRE = ON       // MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
+
+// CONFIG4L
+#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config LVP = OFF        // Single-Supply ICSP Enable bit (Single-Supply ICSP disabled)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
+
+// CONFIG5L
+#pragma config CP0 = OFF        // Code Protection bit (Block 0 (000800-001FFFh) not code-protected)
+#pragma config CP1 = OFF        // Code Protection bit (Block 1 (002000-003FFFh) not code-protected)
+#pragma config CP2 = OFF        // Code Protection bit (Block 2 (004000-005FFFh) not code-protected)
+#pragma config CP3 = OFF        // Code Protection bit (Block 3 (006000-007FFFh) not code-protected)
+
+// CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot block (000000-0007FFh) not code-protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM not code-protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF       // Write Protection bit (Block 0 (000800-001FFFh) not write-protected)
+#pragma config WRT1 = OFF       // Write Protection bit (Block 1 (002000-003FFFh) not write-protected)
+#pragma config WRT2 = OFF       // Write Protection bit (Block 2 (004000-005FFFh) not write-protected)
+#pragma config WRT3 = OFF       // Write Protection bit (Block 3 (006000-007FFFh) not write-protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot block (000000-0007FFh) not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 (000800-001FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 (002000-003FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR2 = OFF      // Table Read Protection bit (Block 2 (004000-005FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR3 = OFF      // Table Read Protection bit (Block 3 (006000-007FFFh) not protected from table reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) not protected from table reads executed in other blocks)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+/* include */      
 # include <xc.h>
 # include <proc/pic18f4520.h> 
 
-/* definitions */
-# define _XTAL_FREQ 8000000
+/* definition */
+#define _XTAL_FREQ 8000000
 
-# define CCP1 LATCbits.LATC2
+int b;
 
-void PWM_init(void)
+void init(void)
 {
-    TRISC = 0x00;
-    CCP1 = 0;
-    // 設定TIMER2, prescaler為4
-    T2CON = 0b01111101;
+    TRISBbits.RB0 = 1;  // input
+    TRISD = 0x00;  // output
+    LATD = 0;
+    b = 0;
+    
+    RCONbits.IPEN = 1;
+    INTCONbits.GIEH = 1;
+    INTCONbits.GIEL = 1;
+    INTCONbits.INT0IE = 1;
+    INTCONbits.INT0IF = 0;
+}
 
-    // 設定OSC頻率，Fosc為125k = Tosc為8µs
-    OSCCONbits.IRCF = 0b001;
+int main(void) {
+    init();
+    while(1);
     
-    // 設定CCP1
-    CCP1CONbits.CCP1M = 0b1100;
-    
-    // 設定pr2和ccp的數字來調整輸出的PWM週期和Duty Cycle
-    // period = (PR2+1)*4*Tosc*(TMR2 prescaler) = (0x9b + 1) * 4 * 8µs * 4 = 0.019968s ~= 20ms
-    PR2 = 0x9b;
-    
-    // 500µs ~= 8µs * 4 * (0x04 * 4 + 0)
-    CCPR1L = 0x04;
-    CCP1CONbits.DC1B = 0b00;
+    return 0;
+}
+
+void __interrupt(high_priority) Hi_ISR(void)
+{
+    if(b == 1){
+        b = 0;
+        PORTD = 0;
+    }
+    else{
+        b = 1;
+        PORTD = 1;
+    }
+    INTCONbits.INT0IF = 0;
+}
+
+void __interrupt(low_priority) Lo_ISR(void) {
+     return;
 }
